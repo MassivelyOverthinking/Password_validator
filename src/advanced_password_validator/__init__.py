4ce401ed3c46e7d138d06cb9e0274065d18d87b1
Simon Stensgaard <STcena1@hotmail.com>
2025-05-30 19:15:47 +0200

    Fixed minor issues

--- src/advanced_password_validator/__init__.py
+++ src/advanced_password_validator/__init__.py
@@ -1,6 +1,6 @@
 #-------------------- Package Management --------------------
 
 __all__ = ["mode", "validator"]
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __author__ = "HysingerDev"
 
--- src/advanced_password_validator/data/__init__.py
+++ src/advanced_password_validator/data/__init__.py
@@ -1,5 +1,5 @@
 #-------------------- Package Management --------------------
 
 __all__ = ["blacklist", "common_passwords"]
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __author__ = "HysingerDev"
